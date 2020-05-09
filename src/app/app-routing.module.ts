c9d94cc2bdd3dbbf3d186314386bcf385b2ef2ec
jaybenaim <benaimjacob@gmail.com>
2020-05-09 19:27:57 -0400

    initial commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,16 @@
+﻿import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: '',
+    loadChildren: () => import('./app-shell/home/home.module').then(module => module.HomeModule)
+  },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule { }
+
---
+++ src/app/components/home/home.component.ts
@@ -0,0 +1,12 @@
+﻿import { Component, OnInit } from "@angular/core";
+
+@Component({
+  selector: "app-blank",
+  templateUrl: "./home.component.html",
+  styleUrls: ["./home.component.css"],
+})
+export class HomeComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
