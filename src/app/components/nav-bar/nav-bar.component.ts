39300c9c62184965fdb887bb4bb4b25c618941c0
jaybenaim <benaimjacob@gmail.com>
2020-05-13 20:40:18 -0400

    added login button to navbar

--- src/app/components/nav-bar/nav-bar.component.ts
+++ src/app/components/nav-bar/nav-bar.component.ts
@@ -1,14 +1,21 @@
-﻿import { Component, OnInit } from '@angular/core';
-import { Location } from '@angular/common';
+﻿import { Component, OnInit } from "@angular/core";
+import { Location } from "@angular/common";
+import { AuthService } from "src/app/services/auth.service";
 
 @Component({
-  selector: 'app-nav-bar',
-  templateUrl: './nav-bar.component.html',
-  styleUrls: ['./nav-bar.component.css']
+  selector: "app-nav-bar",
+  templateUrl: "./nav-bar.component.html",
+  styleUrls: ["./nav-bar.component.css"],
 })
 export class NavBarComponent implements OnInit {
   skipLinkPath: string;
-  constructor(private location: Location) { }
+  showAccount: boolean = false;
+
+  constructor(private location: Location, public authService: AuthService) {}
+
+  toggleShowAccount() {
+    this.showAccount = !this.showAccount;
+  }
 
   ngOnInit() {
     this.skipLinkPath = `${this.location.path()}#mainContent`;
