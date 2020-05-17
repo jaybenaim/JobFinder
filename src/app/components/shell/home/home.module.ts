1229e8e1f62d283b70c24feaa9ba2880b2e38d56
jaybenaim <benaimjacob@gmail.com>
2020-05-17 14:49:10 -0400

    style: stretched search bar to full page

--- src/app/components/shell/home/home.module.ts
+++ src/app/components/shell/home/home.module.ts
@@ -4,8 +4,7 @@ import { CommonModule } from "@angular/common";
 import { HomeComponent } from "./home.component";
 import { HomeRoutingModule } from "./home-routing.module";
 import { SavedJobsComponent } from "../../jobs/saved-jobs/saved-jobs.component";
-import { AllJobsComponent } from "../../jobs/all-jobs/all-jobs.component";
-import { SearchBarComponent } from "../search-bar/search-bar.component";
+import { SearchBarComponent } from "../../search-bar/search-bar.component";
 
 @NgModule({
   declarations: [HomeComponent, SavedJobsComponent, SearchBarComponent],
