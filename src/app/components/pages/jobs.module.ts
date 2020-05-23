2ebb48c239ca7b6d873a3b7b044ef7cf45209ffc
jaybenaim <benaimjacob@gmail.com>
2020-05-22 21:39:01 -0400

    removed extra component

--- src/app/components/pages/jobs.module.ts
+++ src/app/components/pages/jobs.module.ts
@@ -14,7 +14,6 @@ import { IndeedComponent } from "./IndeedPage/indeed/indeed.component";
 import { IndeedPaginationComponent } from "./IndeedPage/indeed-pagination/indeed-pagination.component";
 import { IndeedResultComponent } from "./IndeedPage/indeed-result/indeed-result.component";
 import { HomeComponent } from "./home/home.component";
-import { SavedJobResultComponent } from './SavedJobsPage/saved-job-result/saved-job-result.component';
 
 @NgModule({
   declarations: [
@@ -30,7 +29,6 @@ import { SavedJobResultComponent } from './SavedJobsPage/saved-job-result/saved-
     IndeedPaginationComponent,
     IndeedResultComponent,
     HomeComponent,
-    SavedJobResultComponent,
   ],
   imports: [CommonModule, JobsRoutingModule],
   exports: [],
